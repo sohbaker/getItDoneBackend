ee8efd1c41cb25deed0eb3250a32bfec3211506c
Tola's Terminal <sohbaker@gmail.com>
2020-01-08 14:33:15 +0000

    specifies request resource

--- src/main/kotlin/com/sample/controller/TodoController.kt
+++ src/main/kotlin/com/sample/controller/TodoController.kt
@@ -5,7 +5,9 @@ import com.sample.repository.TodoRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.CrossOrigin
 
+@CrossOrigin(origin = "http://localhost:4523")
 @RestController
 class TodoController {
     @Autowired
