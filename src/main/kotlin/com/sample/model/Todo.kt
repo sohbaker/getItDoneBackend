2cc536b132e3adf207b745e5bed94dcd564032df
Tola's Terminal <sohbaker@gmail.com>
2020-01-08 14:32:54 +0000

    adds completed field to todo class

--- src/main/kotlin/com/sample/model/Todo.kt
+++ src/main/kotlin/com/sample/model/Todo.kt
@@ -1,3 +1,3 @@
 package com.sample.model
 
-data class Todo(var id: Int, var name: String)
+data class Todo(var id: Int, var name: String, var completed: Boolean)
--- src/main/kotlin/com/sample/repository/TodoRepository.kt
+++ src/main/kotlin/com/sample/repository/TodoRepository.kt
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Repository
 @Repository
 class TodoRepository {
     val todos = mutableListOf(
-            Todo(id = 1, name = "drink water"),
-            Todo(id = 2, name = "feed the cat"),
-            Todo(id = 3, name = "buy food")
+            Todo(id = 1, name = "drink water", completed = false),
+            Todo(id = 2, name = "feed the cat", completed = false),
+            Todo(id = 3, name = "buy food", completed = false)
     )
 
     fun findAll(): MutableList<Todo> {
