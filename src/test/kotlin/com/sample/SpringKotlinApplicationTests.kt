ebed37f09bc7b7bcd6c4a5246631a8a289322d29
Tola's Terminal <sohbaker@gmail.com>
2020-02-28 11:19:30 +0000

    updates test

--- src/test/kotlin/com/sample/SpringKotlinApplicationTests.kt
+++ src/test/kotlin/com/sample/SpringKotlinApplicationTests.kt
@@ -1,12 +1,24 @@
 package com.sample
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
 import org.springframework.test.context.ActiveProfiles
 
 @ActiveProfiles("test")
+@SpringBootTest(classes = arrayOf(SpringKotlinApplication::class), webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringKotlinApplicationTest {
+	@Autowired
+	lateinit var testRestTemplate: TestRestTemplate
+
 	@Test
 	fun loadsApplicationSuccessfully() {
-		println("Loaded successfully")
+		val result = testRestTemplate
+				.getForEntity("/", String::class.java)
+
+		assertEquals(HttpStatus.OK, result?.statusCode)
 	}
 }
